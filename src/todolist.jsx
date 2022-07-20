310ab41b81e33173680dc950b222c79c89830e18
elias <maestrofigueroa1@gmail.com>
2022-07-20 20:09:54 -0300

    se finaliza proyecto

---
+++ src/todolist.jsx
@@ -0,0 +1,12 @@
+
+import React from "react"
+import { Todoitem } from "./todoitem"
+export function Todolist({ todos }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <Todoitem todo = {todo}/>
+      ))}
+    </ul>
+  );
+}
